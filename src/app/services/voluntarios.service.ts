ca2b9f21ca7c94acc0b44522f5f4e0aed4efc87f
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2020-01-31 04:28:25 -0300

    Criado Repositório

---
+++ src/app/services/voluntarios.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Voluntario } from '../data-models/voluntario';
+import { AvatarVoluntario } from '../data-models/avatar-voluntario';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VoluntariosService {
+
+    constructor(public db: AngularFirestore) {}
+
+    getAvatars(){
+        return this.db.collection<AvatarVoluntario>('/avatar').valueChanges()
+    }
+
+    geVoluntario(uuid){
+      return this.db.collection('voluntarios').doc<Voluntario>(uuid).snapshotChanges();
+    }
+
+    updateVoluntario(uuid: string, voluntario: Voluntario){
+      voluntario.nameToSearch = voluntario.nome.toLowerCase();
+      return this.db.collection('voluntarios').doc<Voluntario>(uuid).set(voluntario);
+    }
+
+    deleteVoluntario(uuid: string){
+      return this.db.collection('voluntarios').doc<Voluntario>(uuid).delete();
+    }
+
+    getUsers(){
+      return this.db.collection('voluntarios').snapshotChanges();
+    }
+
+    searchVoluntario(searchValue: string){
+      return this.db.collection('voluntarios',ref => ref.where('nameToSearch', '>=', searchValue)
+        .where('nameToSearch', '<=', searchValue + '\uf8ff'))
+        .snapshotChanges();
+    }
+
+    searchVoluntarioPelaIdade(idade){
+      return this.db.collection('voluntarios',ref => ref.orderBy('idade').startAt(idade)).snapshotChanges();
+    }
+
+    createVoluntario(voluntario: Voluntario, avatar: AvatarVoluntario){
+      return this.db.collection('voluntarios').add({
+        nome: voluntario.nome,
+        nameToSearch: voluntario.nome.toLowerCase(),
+        sobrenome: voluntario.sobrenome,
+        idade: parseInt(voluntario.idade, 10),
+        avatar
+      });
+    }
+  }
+
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  firebase: {
+    hosting: 'streetpet',
+    apiKey: 'AIzaSyDxbkicoGy26nUK4NdjILbxzPQcLEc3sTQ',
+    authDomain: 'street-pet.firebaseapp.com',
+    databaseURL: 'https://street-pet.firebaseio.com',
+    projectId: 'street-pet',
+    storageBucket: 'street-pet.appspot.com',
+    messagingSenderId: '547302969236',
+    appId: '1:547302969236:web:22da3603c04457d8900981',
+    measurementId: 'G-LNHTQ959Q1'
+  }
+};
