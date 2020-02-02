1786c96dd713d2dd9f6c20e0e2429205dc5dccbd
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2020-02-02 04:28:48 -0300

    Data model e Serviços movidos para biblioteca entidades
    Arquivos de entidade e serviços relacionados movido para biblioteca
    especifica de dados.

--- src/app/data-models/voluntario.ts
+++
@@ -1,9 +0,0 @@
-import { AvatarVoluntario } from './avatar-voluntario';
-export interface Voluntario {
-  idade: string;
-  sobrenome: any;
-  nameToSearch: string;
-   uuid: string;
-   nome: string;
-   avatar: AvatarVoluntario;
-}
