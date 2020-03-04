12c3a43204e8d98fe610f3c8de1c35dd764dc16a
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2020-03-04 09:46:42 -0300

    Adoção do material icons e outras formatações de código

--- src/app/avatar-dialog/avatar-dialog.component.ts
+++ src/app/avatar-dialog/avatar-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material'
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { AvatarVoluntario } from 'projects/entities/src/lib/voluntarios';
 import { VoluntariosService } from 'projects/entities/src/lib/voluntarios.service';
 import { Subscription, Observable } from 'rxjs';
@@ -30,8 +30,8 @@ export class AvatarDialogComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     if (this.subscription && !this.subscription.closed) {
-      this.subscription.unsubscribe()
-    };
+      this.subscription.unsubscribe();
+    }
   }
 
   getAvatars(): Observable<AvatarVoluntario[]> {
